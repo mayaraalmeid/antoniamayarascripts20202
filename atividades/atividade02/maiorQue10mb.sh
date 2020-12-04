b7cd0114825fe52c923c55bee093150676ae795c
mayaraalmeid <69010544+mayaraalmeid@users.noreply.github.com>
2020-12-04 19:19:02 -0300

    Update maiorQue10mb.sh

--- atividades/atividade02/maiorQue10mb.sh
+++ atividades/atividade02/maiorQue10mb.sh
@@ -1,6 +1,6 @@
 cat > maiorQue10mb.sh
 mkdir maiorQue10
-find teste/ -size +10M -exec mv {} /home/mayara/maiorQue10 \;
+find -size +10M -exec mv {} /maiorQue10 \;
 tar -czvf maiorQue10.tar.gz maiorQue10/
 rm -r maiorQue10
 ^D
