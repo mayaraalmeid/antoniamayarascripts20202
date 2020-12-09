8d0552ee775b8946ce2a9c208b1f7e56fe191f63
João Marcelo Uchôa de Alencar <jmarcelo.alencar@gmail.com>
2020-12-08 21:52:28 -0300

    Update parte_01.sh
    Correção da Parte 01.

--- execicios/exercicio2/parte_01.sh
+++ execicios/exercicio2/parte_01.sh
@@ -1,4 +1,6 @@
+# Qual o papel deste cat?
 cat > parte_01.sh
+# O resto parece correto.
 grep -E '\bA' emailsordenados.txt
 grep -E '.br$' emailsordenados.txt
 grep -E '[0-9][^0-9 ]' emailsordenados.txt
