80f21e9607ac7fcac9572d41fbc2e49da2f47d67
mayaraalmeid <69010544+mayaraalmeid@users.noreply.github.com>
2020-12-15 21:18:06 -0300

    Update saudacao.sh

--- execicios/exercicio03/saudacao.sh
+++ execicios/exercicio03/saudacao.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Olá $(whoami),
-Hoje é dia $(date +%d) do mês $(date +%m) do ano de $(date +%G)">>saudacao.log
+Hoje é dia $(date +%d) do mês $(date +%m) do ano de $(date +%G)">>saudacao.log | tee -a saudacao.log
