94bfd6b12cd85fb94086aacd01879fb7e41f958e
root <root@DESKTOP-98AB1V0.localdomain>
2020-12-28 14:18:35 -0300

    adicionando atividade5

---
+++ atividades/atividade05/agenda.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+comando=${1}
+nome=${2}
+email=${3}
+
+if test ${comando} = 'adicionar'
+then
+         echo ${nome}:${email} >> usuarios.db
+         echo  "Contato ${nome} adicionado com sucesso"
+fi
+
+if test ${comando} = 'listar'
+then 
+        cat usuarios.db
+fi
+
+if test ${comando} = 'remover'
+then
+        sed -i '/'${nome}'/d' usuarios.db
+fi 
