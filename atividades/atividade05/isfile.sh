6658c87eaa4d72f109be43835c45005475061fef
root <root@DESKTOP-98AB1V0.localdomain>
2020-12-28 14:52:49 -0300

    enviando atividade5

---
+++ atividades/atividade05/isfile.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+tipo=${1}
+
+if test -f ${tipo}
+then 
+       echo "É um arquivo."
+
+else 
+      echo "É um diretorio."
+fi
+
+if test -r ${tipo}
+then
+      echo "Tem permissão de leitura."
+else
+      echo "Não tem permissao de leitura."
+fi
+
+if test -w ${tipo}
+then 
+      echo "Tem permissão de escrita."
+else
+      echo "Não tem permissão de escrita."
+fi
