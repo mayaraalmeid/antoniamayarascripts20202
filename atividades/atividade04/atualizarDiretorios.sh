3dfb455ff8123880be5ca054f6069f32826d0003
João Marcelo Uchôa de Alencar <jmarcelo.alencar@gmail.com>
2020-12-28 08:45:37 -0300

    Correção.

--- atividades/atividade04/atualizarDiretorios.sh
+++ atividades/atividade04/atualizarDiretorios.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+# Correção: 0,5
+
+# Para que tantos ..? Bastava /etc/passwd. Outra coisa, o primeiro comando troca todos os "home" por "srv". O "/home/ubuntu" seria trocado por "/srv/ubuntu", o que não deve
+# ocorrer.
 sed 's/home/srv/g' ../../../../../../etc/passwd | sed 's/alunos/students/g' > passwd.new
