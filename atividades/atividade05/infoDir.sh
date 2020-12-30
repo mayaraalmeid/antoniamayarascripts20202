4a36f9fe2fb452ae97eb0f4a4ab1ebaf38132c25
root <root@DESKTOP-98AB1V0.localdomain>
2020-12-30 15:31:55 -0300

    atividade5

--- atividades/atividade05/infoDir.sh
+++ atividades/atividade05/infoDir.sh
@@ -3,7 +3,7 @@ diretorio=${1}
 
 if test -d ${diretorio}
 then
-       echo "O diretorio ${diretorio} ocupa $(du -s ${diretorio} | cut -f1 -d'/')kilobytes e tem $(ls  ${diretorio} | wc -l ) itens."
+       echo "O diretorio ${diretorio} ocupa $(du -s ${diretorio}|sed 's/[[:space:]]/:/g' | cut -f1 -d':') kilobytes e tem $(ls  ${diretorio} | wc -l ) itens."
 else
        echo "${diretorio} não é um diretorio."
 fi
