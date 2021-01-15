f4f4fcd532e5f33f70c434357dd317e68e917ed2
root <root@DESKTOP-98AB1V0.localdomain>
2021-01-15 19:03:27 -0300

    atividade7

---
+++ atividades/atividade07/ipsLatencia.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ips=${1}
+echo "Relatório de Latência."
+for i in $(cat ${ips})
+do
+     ping -c 5 "$i" >> resultIp.txt
+     echo "$i $(grep avg resultIp.txt | cut -f5 -d'/')ms"
+done
+rm resultIp.txt
