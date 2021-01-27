6afa5df1a33293e030ca260f02b89a27baa41dc6
root <root@DESKTOP-98AB1V0.localdomain>
2021-01-27 15:08:08 -0300

    atividade9

---
+++ atividades/atividade09/alertaDiretorio.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+temp=${1}
+dir=${2}
+
+while true
+do
+qnti=$(ls ${dir} | wc -l)
+sleep $temp
+qnta=$(ls ${dir} | wc -l)
+data=$(stat ${dir1}/ |  grep Modify | cut -f1 -d'.' |sed "s/Modify://g")
+    if [[ $qnti > $qnta ]]
+    then
+       echo "[$data] Alteração! $qnti->$qnta. Removidos: " |tee -a dirSensors.log
+    fi
+    if [[ $qnti < $qnta ]]
+     then
+          echo "[$data] Alteração! $qnti->$qnta. Adicionados: " | tee -a dirSensors.log
+     fi
+done
