847009cd55a966e17c0f90769531b9b022278406
João Marcelo <jmarcelo.alencar@gmail.com>
2021-03-14 09:54:25 -0300

    Correção: 0,2

--- atividades/atividade15/instanciaServico.sh
+++ atividades/atividade15/instanciaServico.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 IMAGEM=”ami-042e8287309f5df032”
 GRUPO=$(aws ec2 create-security-group --group-name “scripts” --description “grupo de seguranca scripts” --output text)
 SUBREDE=$(aws ec2 describe-subnets --query “Subnets[0].SubnetId” --output text)
@@ -9,6 +10,8 @@ INSTANCIA=$(aws ec2 run-instances --image-id $IMAGEM --instance-type t2.micro --
 
 echo “Criando servidor de monitoramento...”
 IP=$(aws ec2 describe-instances --instances-id $iINSTANCIA --query “Reservations[0].Instances[0].PublicIpAddress” --output text)
+# Isto aqui você está fazendo localmente, não na máquina remota. A grande questão das últimas aulas foi aprender a usar
+# o parâmetro user_data para executar scripts nas máquinas virtuais. Isso você demonstrou não compreender.
 sudo su -
 apt install apache2
 echo “Acesse: HTTP://$IP/
