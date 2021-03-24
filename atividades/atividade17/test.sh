355e59cdda4685de29ce44372dfcf19e1435693e
mayaraalmeid <69010544+mayaraalmeid@users.noreply.github.com>
2021-03-24 12:40:07 +0000

    atividade17

---
+++ atividades/atividade17/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+apt update
+apt-get install -y mysql-server
+
+sed -i '31c\bind-address      =0.0.0.0/0'  /etc/mysql/mysql.conf.d/mysqld.cnf
+sed -i '32c\mysqlx-bind-address      =0.0.0.0/0'  /etc/mysql/mysql.conf.d/mysqld.cnf
+
+systemctl restart mysql.service
+
+mysql<<EOF
+CREATE DATABASE scripts;
+CREATE USER '$usuario'@'%' IDENTIFIED BY '$senha';
+EOF
+
