6556043f2a23071fe7f7cbc90e388795d8c6a75f
mayaraalmeid <69010544+mayaraalmeid@users.noreply.github.com>
2021-03-31 16:48:47 +0000

    Update wordpress.sh

--- atividades/atividade18/wordpress.sh
+++ atividades/atividade18/wordpress.sh
@@ -55,13 +55,26 @@ else
 fi
 done
 
+cat<<EOF > test2.sh
+#!/bin/bash
+apt update
+apt-get install -y mysql-client
+apt-get install apache2 php-mysql php-curl libapache2-mod-php php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip
+cat<<AP > /etc/apache2/sites-available/wordpress.conf
+<Directory /var/www/html/wordpress/>
+    AllowOverride All
+</Directory>
+AP
 
+a2enmod rewrite
+a2ensite wordpress
+curl -O https://wordpress.org/latest.tar.gz
+tar xzvf latest.tar.gz
+touch wordpress/.htaccess
+cp -a wordpress/. /var/www/html/wordpress
+chown -R www-data:www-data /var/www/html/wordpress
+find /var/www/html/wordpress/ -type d -exec chmod 750 {} \;
+find /var/www/html/wordpress/ -type f -exec chmod 640 {} \;
+systemctl restart apache2
 
-
-
-
-
-
-
-
-
+EOF
