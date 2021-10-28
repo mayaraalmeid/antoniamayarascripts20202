5e2c5bf05a2b580622c5e9464b0ac7698a635e9b
willantec19 <74991831+willantec19@users.noreply.github.com>
2021-10-28 19:59:48 -0300

    Update saudacao.sh

--- execicios/exercicio03/saudacao.sh
+++ execicios/exercicio03/saudacao.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Correção: OK.
 echo "Olá $(whoami),
-Hoje é dia $(date +%d) do mês $(date +%m) do ano de $(date +%G)" | tee -a saudacao.log
+Hoje é dia $(date +%d), do mês $(date +%m) do ano de $(date +%G)" | tee -a saudacao.log
